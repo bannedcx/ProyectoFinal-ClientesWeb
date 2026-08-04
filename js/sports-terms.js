51a5d03bacf5d416c74a78169c05d326d40fa245
bannedcx <ericenriquevargas20@gmail.com>
2026-08-03 20:34:01 -0400

    Add sports configuration for football and basketball

---
+++ js/sports-terms.js
@@ -0,0 +1,29 @@
+export const SportsConfig = {
+    football: {
+        id: 'football',
+        name: 'Fútbol',
+        icon: '⚽',
+        terms: {
+            event: 'Gol',
+            pointsFor: 'GF', 
+            pointsAgainst: 'GC', 
+            scorersRanking: 'Goleadores',
+            individualScore: 'Goles anotados'
+        },
+        colors: {
+            primary: '#27ae60', 
+            accent: '#f1c40f'
+        }
+    },
+    basketball: {
+        id: 'basketball',
+        name: 'Básquetbol',
+        icon: '🏀',
+        terms: {
+            event: 'Canasta', 
+            pointsFor: 'PF', 
+            pointsAgainst: 'PC', 
+            scorersRanking: 'Encestadores', 
+            individualScore: 'Canastas encestadas'
+        },
+      
